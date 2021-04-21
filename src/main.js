01edb52f1396ec3aca93e94e156e02723ef5ca6b
AvianBen <n2wben@gmail.com>
2021-04-20 23:38:42 -0600

    Added Points in Main

--- src/main.js
+++ src/main.js
@@ -11,3 +11,8 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 let keyLEFT, keyRIGHT, keyF, keyR;
+
+//60 points: change the game's aesthetic, I changed the game's aesthetic to a western theme where you catch snakes in the desert
+//20 points: I made a smaller and faster snake that is worth more points
+//10 points: I made a foreground frame to replace the white borders
+//I tried a couple other mod ideas but I couldn't get them to work, and I also find coding really frustrating so I finished with those three
